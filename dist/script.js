e2c00a510b98a4ae4398cc8d4608a5c9632b0917
AkhilJayan <mailtoakhiljayan@gmail.com>
2018-09-12 01:59:00 +0800

    added fevicon

--- dist/script.js
+++ dist/script.js
@@ -47079,6 +47079,41 @@ $provide.value("$locale", {
 
 })();
 (function () {
+    angular.module('app').component('home', {
+        controller: 'HomeController',
+        controllerAs: 'vm',
+        templateUrl: 'views/home/home.html'
+    });
+})();
+(function () {
+    angular.module('app').controller('HomeController', [
+        '$state', '$scope',
+        function ($state, $scope) {
+            var vm = this;
+            vm.header = 'Home sweet home!';
+            vm.$onInit = onInit;
+    
+            activate();
+    
+            ////////////
+    
+            function activate() {
+                // Resolve start-up logic
+            }
+    
+            vm.test = function(){
+                $state.go('form.input');
+            }
+    
+            function onInit() {
+                // Initialization logic that relies on bindings being present
+                // should be put in this method, which is guarranteed to
+                // always be called after the bindings have been assigned.
+            }
+        }
+    ]);
+})();
+(function () {
     angular.module('app').component('formInput', {
         controller: 'FormInputController',
         controllerAs: 'vm',
@@ -47137,41 +47172,6 @@ $provide.value("$locale", {
             function onInit() {
                 vm.employees = [];
             };
-        }
-    ]);
-})();
-(function () {
-    angular.module('app').component('home', {
-        controller: 'HomeController',
-        controllerAs: 'vm',
-        templateUrl: 'views/home/home.html'
-    });
-})();
-(function () {
-    angular.module('app').controller('HomeController', [
-        '$state', '$scope',
-        function ($state, $scope) {
-            var vm = this;
-            vm.header = 'Home sweet home!';
-            vm.$onInit = onInit;
-    
-            activate();
-    
-            ////////////
-    
-            function activate() {
-                // Resolve start-up logic
-            }
-    
-            vm.test = function(){
-                $state.go('form.input');
-            }
-    
-            function onInit() {
-                // Initialization logic that relies on bindings being present
-                // should be put in this method, which is guarranteed to
-                // always be called after the bindings have been assigned.
-            }
         }
     ]);
 })();
